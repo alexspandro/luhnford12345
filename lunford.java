27fafdfac8cef5815e8499412e858df895f8d67d
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-09 15:31:36 -0400

    Update lunford.java

--- lunford.java
+++ lunford.java
@@ -3,12 +3,13 @@ import java.util.Scanner
 public class luhnford {
   public static void main(string[] args){
   Scanner daScan = new Scanner(System.in);
-
-  folder = os.getcwd()
-  fileName = input("Generating new customer file...\nWhat do you want to name this file? ")
-  folderName = input("Which existing folder would you like to assign the file to? ")
-  fileCreateNew = folder + folderName + "\\" + fileName + ".txt"
-  file = ("")
+  String currentDir = System.getProperty("user.dir");
+  System.out.println("Using cwd: " + currentDir); //gets cwd
+  String
+  String fileName = ("Generating new customer file...\nWhat do you want to name this file? ")
+  String folderName = input("Which existing folder would you like to assign the file to? ")
+  String fileCreateNew = folder + folderName + "\\" + fileName + ".txt"
+  String file = ("")
   try:
     file = open(fileName, "w")
     file.writelines("NEW CUSTOMER PROFILE\n-------------")
