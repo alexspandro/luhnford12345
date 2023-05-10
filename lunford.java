afdc518ff0b4ff458560829cd1ece381c1ea4761
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-10 15:15:03 -0400

    Update lunford.java

--- lunford.java
+++ lunford.java
@@ -1,25 +1,28 @@
-import java.util.Scanner
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class luhnford {
   public static void main(String[] args){
-  Scanner daScan = new Scanner(System.in);
-  String currentDir = System.getProperty("user.dir");
-  System.out.println("Using cwd: " + currentDir); //gets cwd
-  System.out.print("Generating new customer file...\nWhat do you want to name this file? ");
-  String fileName = daScan.nextLine(); 
-  System.out.print("Which existing folder would you like to assign the file to? ");
-  String folderName = daScan.nextLine();
-  String filePath = folderName + "\\" + fileName + ".txt";
-  try:
-    file = open(fileName, "w")
-    file.writelines("NEW CUSTOMER PROFILE\n-------------")
-    file.writelines("First Name: " + CustFirstName + "\n")
-    file.writelines("Last Name: " + CustLastName + "\n")
-    file.writelines("City: " + CustCity + "\n")
-    file.writelines("Postal Code: " + CustPCode + "\n")
-    file.writelines("Credit Card Number: " + CustCCard + "\n")
-    print("FILE SAVED IN FOLDER:", folderName)
-    finally:
-      if file:
-      file.close()
-        
+    Scanner daScan = new Scanner(System.in);
+    String currentDir = System.getProperty("user.dir");
+    System.out.println("Using cwd: " + currentDir); //gets cwd
+    System.out.print("Generating new customer file...\nWhat do you want to name this file? ");
+    String fileName = (daScan.nextLine() + ".csv"); 
+    System.out.print("Which existing folder would you like to assign the file to? ");
+    String folderName = daScan.nextLine();
+    String filePath = folderName + "\\" + fileName;
+    try (FileWriter FileWrite = new FileWriter(fileName)) {
+      FileWrite.write("NEW CUSTOMER PROFILE\n-------------");
+      FileWrite.write("First Name: " + CustFirstName + "\n");
+      FileWrite.write("Last Name: " + CustLastName + "\n");
+      FileWrite.write("City: " + CustCity + "\n");
+      FileWrite.write("Postal Code: " + CustPCode + "\n");
+      FileWrite.write("Credit Card Number: " + CustCCard + "\n");
+      System.out.println("FILE SAVED IN FOLDER:", folderName);
+    catch IOException e) {
+      System.out.println("Error in da process cuh. No capa cuh." + e.getMessage());
+    }
+    }
+  }
+}
