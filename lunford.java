7d29b579930c1e243fa48daddbac5800f795cddb
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-12 14:40:13 -0400

    Update lunford.java

--- lunford.java
+++ lunford.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class luhnford {
+public class lunford {
   public static void main(String[] args){
     Scanner daScan = new Scanner(System.in);
     System.out.println("Using cwd: " + System.getProperty(currentDir); //gets cwd
