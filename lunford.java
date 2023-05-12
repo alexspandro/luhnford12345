c17f676952d535e80dcf3c34f19c1fde14c9d807
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-05-11 21:38:08 -0400

    Update lunford.java

--- lunford.java
+++ lunford.java
@@ -2,4 +2,7 @@ import java.util.Scanner
 
 public class luhnford {
   public static void main(string[] args){
-  Scanner daScan = new Scanner(System.in);
+    Scanner daScan = new Scanner(System.in);
+  public static void printMenu();
+    System.out.println("Customer and Sales System\nEnter Customer Information\nGenerate Customer data file\nReport on total Sales (Not done in this part)\nCheck for fraud in sales data (Not done in this part)\nQuit\nEnter menu option (1-9)")
+                   
