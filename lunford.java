d770eb9a554b8117a807e49b906c1f506b7c32dc
mahirzaman2510 <129217554+mahirzaman2510@users.noreply.github.com>
2023-05-17 21:28:19 -0400

    Update lunford.java
    i added my postal code validation

--- lunford.java
+++ lunford.java
@@ -1,18 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class CreditCardValidationSystem {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a credit card number: ");
-        String creditCardNumber = scanner.nextLine();
-        boolean isValid = isValidCreditCard(creditCardNumber);
-        if (isValid) {
-            System.out.println("The credit card number is valid.");
-        } else {
-            System.out.println("The credit card number is invalid.");
-        }
-        scanner.close();
-    }
+
 
     public static boolean isValidCreditCard(String creditCardNumber) {
         // Check if the credit card number is at least 9 digits in length
@@ -23,7 +13,7 @@ public class CreditCardValidationSystem {
         // Apply the Luhn Algorithm to the credit card number
         int total = 0;
         for (int i = creditCardNumber.length() - 2; i >= 0; i -= 2) {
-            int digit = Integer.parseInt(String.valueOf(creditCardNumber.charAt(i)));
+            int digit = creditCardNumber.charAt(i) - '0';
             int product = digit * 2;
             if (product > 9) {
                 product -= 9;
@@ -31,11 +21,39 @@ public class CreditCardValidationSystem {
             total += product;
         }
         for (int i = creditCardNumber.length() - 1; i >= 0; i -= 2) {
-            int digit = Integer.parseInt(String.valueOf(creditCardNumber.charAt(i)));
+            int digit = creditCardNumber.charAt(i) - '0';
             total += digit;
         }
 
         // Check if the total modulo 10 is equal to zero
         return total % 10 == 0;
+    }
+
+    public static boolean isValidPostalCode(String postalCode) {
+        // Check if the postal code has at least 3 characters
+        if (postalCode.length() < 3) {
+            return false;
+        }
+
+        // Open the postal codes file and search for a match
+        File file = new File("postal_codes.csv");
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("Postal codes file not found.");
+            return false;
+        }
+        boolean matchFound = false;
+        while (scanner.hasNextLine() && !matchFound) {
+            String line = scanner.nextLine();
+            if (line.startsWith(postalCode.substring(0, 3))) {
+                matchFound = true;
+            }
+        }
+        scanner.close();
+
+        // If a match is found, the postal code is valid
+        return matchFound;
     }
 }
